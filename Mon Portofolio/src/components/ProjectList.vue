e5f3607cfbfd32e149e746a5193dc1ad389537b8
Chirack96 <jacques-chirack.gougou@epitech.eu>
2024-04-28 16:09:03 +0200

    Refactor ProjectList.vue component to use rounded corners for ProjectCard

--- Mon Portofolio/src/components/ProjectList.vue
+++ Mon Portofolio/src/components/ProjectList.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-wrap justify-center">
       <div v-for="(project, index) in projects" :key="project.id" class="w-full md:w-1/3 p-4"
         :class="{ 'animate-slideInLeft': index % 2 === 0, 'animate-slideInRight': index % 2 !== 0 }">
-        <ProjectCard class="w-full shadow-lg text-lg" :name="project.name" :description="project.description"
+        <ProjectCard class="w-full shadow-lg text-lg rounded-xl" :name="project.name" :description="project.description"
           :image="project.image" />
       </div>
     </div>
